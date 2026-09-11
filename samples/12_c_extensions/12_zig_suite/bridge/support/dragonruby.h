3c01e412dcd10b14d1f70004df31045230867759
Johnneylee Jack Rollins <git@spacegho.st>
2026-09-11 09:14:41 -0700

    Extend test host with supported draw dispatch API

--- samples/12_c_extensions/12_zig_suite/bridge/support/dragonruby.h
+++ samples/12_c_extensions/12_zig_suite/bridge/support/dragonruby.h
@@ -12,12 +12,12 @@
 #include <stdint.h>
 #define DRB_FFI_EXPORT __attribute__((visibility("default")))
 typedef mrb_int drbz_get_args_fn(mrb_state *, const char *, ...);
-/* mruby 3.0 uses size_t here; 3.4 uses mrb_int. Derive the real signature
- * from this VM's header rather than casting incompatible function pointers. */
 typedef __typeof__(mrb_str_new) drbz_str_new_fn;
 typedef __typeof__(mrb_ary_new) drbz_ary_new_fn;
 typedef __typeof__(mrb_ary_push) drbz_ary_push_fn;
-/* This is a host contract, NOT the proprietary DragonRuby SDK ABI. */
+typedef __typeof__(mrb_funcall_id) drbz_funcall_id_fn;
+/* This is a host contract, NOT the proprietary DragonRuby SDK ABI. Fields are
+ * limited to APIs already exercised by shipped C-extension samples. */
 typedef struct drb_api_t {
     struct RClass *(*mrb_module_get)(mrb_state *, const char *);
     struct RClass *(*mrb_define_module_under)(mrb_state *, struct RClass *, const char *);
@@ -30,5 +30,7 @@ typedef struct drb_api_t {
     drbz_str_new_fn *mrb_str_new;
     drbz_ary_new_fn *mrb_ary_new;
     drbz_ary_push_fn *mrb_ary_push;
+    mrb_sym (*mrb_intern_cstr)(mrb_state *, const char *);
+    drbz_funcall_id_fn *mrb_funcall_id;
 } drb_api_t;
 #endif
